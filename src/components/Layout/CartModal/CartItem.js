cf43fc3c23294608c8dfb295de606bf782f277ef
Ádrian <adrian.fr.fe@gmail.com>
2021-11-19 18:31:46 -0400

    feat: implement the add one and remove one buttons

--- src/components/Layout/CartModal/CartItem.js
+++ src/components/Layout/CartModal/CartItem.js
@@ -1,10 +1,27 @@
+import { useContext } from 'react';
 import { FaMinus, FaPlus } from 'react-icons/fa';
+import CartContext from '../../../store/cart-context';
 import Button from '../../UI/Button/Button';
 import Price from '../../UI/Price/Price';
 import styles from './CartItem.module.css';
 
 export default function CartItem(props) {
-  const { name, price, quantity } = props.data;
+  const { id, name, price, quantity } = props.data;
+
+  const cartContext = useContext(CartContext);
+
+  function handleRemove() {
+    cartContext.removeItem(id, 1);
+  }
+
+  function handleAdd() {
+    cartContext.addItem({
+      id,
+      name,
+      price,
+      amount: 1,
+    });
+  }
 
   return (
     <li className={styles.item}>
@@ -16,10 +33,10 @@ export default function CartItem(props) {
         </div>
       </div>
       <div className={styles.actions}>
-        <Button className={styles['plus']}>
+        <Button className={styles['plus']} onClick={handleAdd}>
           <FaPlus />
         </Button>
-        <Button className={styles['minus']}>
+        <Button className={styles['minus']} onClick={handleRemove}>
           <FaMinus />
         </Button>
       </div>
