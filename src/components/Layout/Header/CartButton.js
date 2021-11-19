1aa24ac3784fc8f6a6060255bce8e96b71946ea4
Ádrian <adrian.fr.fe@gmail.com>
2021-11-19 18:23:26 -0400

    feat: animate the button on add item to the cart and display the quantity of items

--- src/components/Layout/Header/CartButton.js
+++ src/components/Layout/Header/CartButton.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import CartContext from '../../../store/cart-context';
 
 import styles from './CartButton.module.css';
 
@@ -9,13 +10,34 @@ export default function CartButton(props) {
     animateButton ? styles['bump'] : ''
   }`;
 
+  const cartContext = useContext(CartContext);
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) return;
+
+    setAnimateButton(true);
+
+    const timeout = setTimeout(() => {
+      setAnimateButton(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [cartContext.items]);
+
+  const quantity = cartContext.items.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
+
   return (
     <button className={cartButtonClasses} onClick={props.onClick}>
       <span>
         <FaShoppingCart />
         <span className={styles['cart-text']}>Your Cart</span>
       </span>
-      <div className={styles['cart-count']}>0</div>
+      <div className={styles['cart-count']}>{quantity}</div>
     </button>
   );
 }
