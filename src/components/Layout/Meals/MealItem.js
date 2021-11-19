f775fd71f1d61a0d6f067e24e62f7e8229215fdd
Ádrian <adrian.fr.fe@gmail.com>
2021-11-19 18:22:30 -0400

    feat: implement the logic to add items to the cart

--- src/components/Layout/Meals/MealItem.js
+++ src/components/Layout/Meals/MealItem.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { FaPlus } from 'react-icons/fa';
+import CartContext from '../../../store/cart-context';
 import Button from '../../UI/Button/Button';
 import Input from '../../UI/Input/Input';
 import Price from '../../UI/Price/Price';
@@ -9,6 +10,14 @@ export default function MealItem(props) {
   const { id, name, description, price } = props.data;
 
   const [amount, setAmount] = useState(1);
+  const cartContext = useContext(CartContext);
+
+  function handleAddToCart() {
+    if (amount <= 0) return;
+
+    const data = { id, name, price, amount };
+    cartContext.addItem(data);
+  }
 
   return (
     <li className={styles.item}>
@@ -24,9 +33,9 @@ export default function MealItem(props) {
           type="number"
           value={amount}
           min="1"
-          onChange={({ target }) => setAmount(target.value)}
+          onChange={({ target }) => setAmount(+target.value)}
         />
-        <Button>
+        <Button onClick={handleAddToCart}>
           <FaPlus />
           <span>Add</span>
         </Button>
