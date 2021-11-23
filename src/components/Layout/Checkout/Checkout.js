07b3653a7669da6f8be91bc641188bb0bfdb99a8
Ádrian <adrian.fr.fe@gmail.com>
2021-11-23 15:07:19 -0400

    feat: send order to database

--- src/components/Layout/Checkout/Checkout.js
+++ src/components/Layout/Checkout/Checkout.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef, useState } from 'react';
 import CartContext from '../../../store/cart-context';
 import Modal from '../../UI/Modal/Modal';
 import Price from '../../UI/Price/Price';
+import Loading from '../../UI/Loading/Loading';
 import styles from './Checkout.module.css';
 
 const hasLength = (text, length) => text.trim().length >= length;
@@ -49,83 +50,104 @@ export default function Checkout(props) {
       city: cityIsValid,
     });
 
-    if (!formIsValid) {
-      console.log('Submitted');
-      return;
-    }
+    if (!formIsValid) return;
+
+    props.onConfirm({
+      name,
+      street,
+      postalCode,
+      city,
+    });
   }
+
+  const checkoutContent = (
+    <div className={styles.content}>
+      <form onSubmit={handleSubmit} id="checkout-form">
+        <div className={styles['input-box']}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            ref={nameInputRef}
+            className={!formInputsValid.name ? styles['input-error'] : ''}
+          />
+        </div>
+
+        <div className={styles['input-box']}>
+          <label htmlFor="street">Street</label>
+          <input
+            type="text"
+            id="street"
+            ref={streetInputRef}
+            className={!formInputsValid.street ? styles['input-error'] : ''}
+          />
+        </div>
+
+        <div className={styles['input-box']}>
+          <label htmlFor="postal-code">Postal Code</label>
+          <input
+            type="text"
+            id="postal-code"
+            ref={postalCodeInputRef}
+            className={!formInputsValid.postalCode ? styles['input-error'] : ''}
+          />
+        </div>
+
+        <div className={styles['input-box']}>
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            id="city"
+            ref={cityInputRef}
+            className={!formInputsValid.city ? styles['input-error'] : ''}
+          />
+        </div>
+      </form>
+      <div className={styles['order-info']}>
+        <h3>Order</h3>
+        <ul>
+          {items.length > 0 &&
+            items.map((item) => (
+              <li key={item.id}>
+                <span>
+                  {item.name}{' '}
+                  <span className={styles.quantity}>x{item.quantity}</span>
+                </span>
+                <Price className={styles.price} price={item.price} />
+              </li>
+            ))}
+        </ul>
+        <div className={styles.total}>
+          <span>Total: </span>
+          <Price price={total} />
+        </div>
+      </div>
+    </div>
+  );
+
+  const loadingContent = (
+    <div className={styles.loading}>
+      <Loading />
+    </div>
+  );
+
+  const didSubmitContent = (
+    <div className={styles.success}>
+      <h3>Order submitted!</h3>
+    </div>
+  );
 
   return (
     <Modal
       onClose={props.onClose}
       className={styles.checkout}
-      actionButtonText="Confirm"
+      actionButtonText="Finish"
       actionButtonProps={{ form: 'checkout-form' }}
+      onConfirm={props.didSubmit ? props.onClose : null}
     >
-      <div className={styles.content}>
-        <form onSubmit={handleSubmit} id="checkout-form">
-          <div className={styles['input-box']}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              ref={nameInputRef}
-              className={!formInputsValid.name ? styles['input-error'] : ''}
-            />
-          </div>
-
-          <div className={styles['input-box']}>
-            <label htmlFor="street">Street</label>
-            <input
-              type="text"
-              id="street"
-              ref={streetInputRef}
-              className={!formInputsValid.street ? styles['input-error'] : ''}
-            />
-          </div>
-
-          <div className={styles['input-box']}>
-            <label htmlFor="postal-code">Postal Code</label>
-            <input
-              type="text"
-              id="postal-code"
-              ref={postalCodeInputRef}
-              className={
-                !formInputsValid.postalCode ? styles['input-error'] : ''
-              }
-            />
-          </div>
-
-          <div className={styles['input-box']}>
-            <label htmlFor="city">City</label>
-            <input
-              type="text"
-              id="city"
-              ref={cityInputRef}
-              className={!formInputsValid.city ? styles['input-error'] : ''}
-            />
-          </div>
-        </form>
-        <div className={styles['order-info']}>
-          <h3>Order</h3>
-          <ul>
-            {items.length > 0 &&
-              items.map((item) => (
-                <li key={item.id}>
-                  <span>
-                    {item.name}{' '}
-                    <span className={styles.quantity}>x{item.quantity}</span>
-                  </span>
-                  <Price className={styles.price} price={item.price} />
-                </li>
-              ))}
-          </ul>
-          <div className={styles.total}>
-            <span>Total: </span>
-            <Price price={total} />
-          </div>
-        </div>
-      </div>
+      {props.isLoading && loadingContent}
+      {props.didSubmit && didSubmitContent}
+      {!props.isLoading && !props.didSubmit && checkoutContent}
     </Modal>
   );
 }
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import CartButton from './CartButton';
 
 import styles from './Header.module.css';
 import CartModal from '../CartModal/CartModal';
 import Checkout from '../Checkout/Checkout';
+import CartContext from '../../../store/cart-context';
 
 export default function Header() {
   const [showCart, setShowCart] = useState(false);
   const [showCheckout, setShowCheckout] = useState(false);
+  const { items } = useContext(CartContext);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
 
   function handleCartClick() {
     setShowCart(true);
@@ -26,6 +31,22 @@ export default function Header() {
     setShowCheckout(false);
   }
 
+  async function handleCheckoutConfirm(user) {
+    setIsLoading(true);
+    await fetch(
+      'https://react-food-21b62-default-rtdb.firebaseio.com/orders.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user,
+          items,
+        }),
+      }
+    );
+    setIsLoading(false);
+    setDidSubmit(true);
+  }
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>ReactFood</h1>
@@ -33,7 +54,14 @@ export default function Header() {
       {showCart && (
         <CartModal onClose={handleCartClose} onConfirm={handleCartConfirm} />
       )}
-      {showCheckout && <Checkout onClose={handleCheckoutClose} />}
+      {showCheckout && (
+        <Checkout
+          onClose={handleCheckoutClose}
+          onConfirm={handleCheckoutConfirm}
+          isLoading={isLoading}
+          didSubmit={didSubmit}
+        />
+      )}
     </header>
   );
 }
